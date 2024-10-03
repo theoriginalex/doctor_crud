8a8c9dac4a63d150546a2425d72289b203eda8b2
El Negro Del WhatsApp <123592684+theoriginalex@users.noreply.github.com>
2024-10-03 14:57:48 -0500

    new

--- doctor_crud/urls.py
+++ doctor_crud/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from aplication.core import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
+    path('', include('aplication.core.urls',namespace='core')),
 ]
